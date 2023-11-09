17aaec04ab81838839d3a01d9087acfdbfe962ba
sion <79098475+classaen7@users.noreply.github.com>
2023-11-09 10:58:55 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,22 +1,23 @@
 def solution(sequence, k):
     # 비내림차순 : 각각의 원소가 바로 앞에 있는 원소보다 크거나 같을 경우
-    # 투포인터로 탐색
+    # 투포인터로 탐색 : 부분합
+    
     s,e = 0,0
-    re = []
+    re = []                                     # 정답 후보를 저장
     summ = sequence[0]
     
     # 합이 크면 : s+1 = summ - sequence[s]
     # 합이 작으면 : e+1 = summ + sequence[e]
     
-    while s <= e < len(sequence):
-        if summ == k:
-            # print(s,e)
-            if not re or re[1]-re[0] > e-s:
-                re = [s,e]
+    while e < len(sequence):
+        if summ == k:                           # 투포인터의 부분합 확인
+            if not re or re[1]-re[0] > e-s:     # 정답 후보와 비교 (길이가 더 짧은지, 시작점에 대한 비교는 불필요)
+                re = [s,e]                      # 갱신
             summ -= sequence[s]
             s += 1
-
-        if summ < k:
+        
+        # 부분합이 k와 다른경우 
+        if summ < k:                        
             e += 1
             if e < len(sequence):
                 summ += sequence[e]

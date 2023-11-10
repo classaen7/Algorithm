ca81380c392f92d35abf8fe5e1fa2b7fac194f7a
sion <79098475+classaen7@users.noreply.github.com>
2023-11-10 16:11:54 +0900

    [level 2] Title: 과제 진행하기, Time: 2.08 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,44 +1,37 @@
 import collections
 
 def solution(plans):
-    def time_to_min(time):
+    def time_to_min(time):                  # 시간을 비교,계산하기 위한 함수
         h,m = map(int,time.split(':'))
         return h*60 + m
     
     ans = []
-    
     plans.sort(key=lambda x:x[1])
     not_finish = []
-    
-    print(plans)
-    
+
     for i in range(0,len(plans)-1):    
         
+        fin_time = time_to_min(plans[i][1])+int(plans[i][2])    #i번째 공부가 끝나는 시간 계산
+        left_time = time_to_min(plans[i+1][1]) - fin_time       #i+1 번째 공부와 비교하여 남는시간 계산
         
-        fin_time = time_to_min(plans[i][1])+int(plans[i][2])
-        # 뒤에거랑 비교
-        left_time = time_to_min(plans[i+1][1]) - fin_time
         
-        print(plans[i][0] , left_time)
+        if left_time < 0:                                       # 남는 시간 없을 때
+            not_finish.append([plans[i][0],abs(left_time)])     # 끝내지 못한 과제로 ["이름",시간] 저장
         
-        if left_time < 0: # 남는 시간 없을 때
-            not_finish.append([plans[i][0],abs(left_time)])
-        
-        else:         #남는 시간 있을 때
+        else:                                                   #남는 시간 있을 때
             ans.append(plans[i][0])
             
-            while not_finish and left_time>0:
-                if left_time >= not_finish[-1][1]:
+            while not_finish and left_time>0:                   #못끝낸 과제 하기
+                if left_time >= not_finish[-1][1]:              #시간 충분
                     left_time -= not_finish[-1][1]
                     ans.append(not_finish.pop()[0])
-                else:
+                else:                                           #시간 부족
                     not_finish[-1][1] -= left_time
                     break
-                
-                
-                
-    ans.append(plans[-1][0])     
-    while not_finish:
+   
+    ans.append(plans[-1][0])                                    #마지막건 바로
+    
+    while not_finish:                                           #스택 남아있으면 넣기
         ans.append(not_finish.pop()[0])
     
     return ans

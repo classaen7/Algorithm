65e0d2d0b0c617f52e47a0c718568640619a849c
sion <79098475+classaen7@users.noreply.github.com>
2024-12-21 00:24:39 +0900

    [level 3] Title: 네트워크, Time: 0.56 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,27 +1,22 @@
 def solution(n, computers):
-    from collections import defaultdict
     answer = 0
     
-    dic = defaultdict(list)
-    for i in range(n):
-        for idx, c in enumerate(computers[i]):
-            if i != idx and c==1: 
-                dic[i].append(idx)
-                
     net = [1] * n
-
+    
     for i in range(n):
-        if net[i] == 1:
+        if net[i]:
             answer += 1
-            
-            deq = [i]
             net[i] = 0
-            
+        
+            deq = [i]
             while deq:
                 node = deq.pop(0)
-                for nxt in dic[node]:
-                    if net[nxt] == 1:
-                        net[nxt] = 0
+                
+                for nxt in range(len(computers[node])):
+                    if computers[node][nxt] == 1 and nxt != node and net[nxt]:
                         deq.append(nxt)
+                        net[nxt] = 0
+        
+    
     
     return answer
